833cf8954468f31fdafceedc10e5e2544db10d0b
Shah Tauheed <shah.tauheed02@gmail.com>
2025-11-03 15:28:16 +0530

    Code of the day

--- JS/Task/Switch_case_question.js
+++ JS/Task/Switch_case_question.js
@@ -1,2 +1,76 @@
 // Take a number (1–7). Print the day of the week.
 
+// let num=2;
+// switch(true){
+//     case num==1:
+//         console.log("Sunday");
+//         break;
+//     case num==2:
+//         console.log("Monday");
+//         break;
+//     case num==3:
+//         console.log("Tuesday");
+//         break;
+//     case num==4:
+//         console.log("Wednesday");
+//         break;
+//     case num==5:
+//         console.log("Thrusday");
+//         break;
+//     case num==6:
+//         console.log("Friday");
+//         break;
+//     case num==7:
+//         console.log("Saturday");
+//         break;
+// }
+
+
+
+
+
+// let month=2;
+// switch(true){
+//     case month==1:
+//         console.log("January");
+//         break;
+//     case month==2:
+//         console.log("February");
+//         break;
+//     case month==3:
+//         console.log("March");
+//         break;
+//     case month==4:
+//         console.log("April");
+//         break;
+//     case month==5:
+//         console.log("May");
+//         break;
+//     case month==6:
+//         console.log("June");
+//         break;
+//     case month==7:
+//         console.log("July");
+//         break;
+//     case month==8:
+//         console.log("August");
+//         break;
+//     case month==9:
+//         console.log("September");
+//         break;
+//     case month==10:
+//         console.log("October");
+//         break;
+//     case month==11:
+//         console.log("November");
+//         break;
+//     case month==12:
+//         console.log("December");
+//         break;
+// }
+
+
+
+
+
+
