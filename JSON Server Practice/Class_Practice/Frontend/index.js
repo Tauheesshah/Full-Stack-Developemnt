c5d1c92add22b772e713cda0467207972f141479
Shah Tauheed <shah.tauheed02@gmail.com>
2025-12-08 11:16:41 +0530

    Code of the Day

--- JSON Server Practice/Class_Practice/Frontend/index.js
+++ JSON Server Practice/Class_Practice/Frontend/index.js
@@ -1,5 +1,6 @@
-let data;
+//npx json-server --watch db.json --port 3001
 
+let data;
 const API="http://localhost:3001/todos";
 
 function myTodosAdd(){
@@ -8,7 +9,7 @@ function myTodosAdd(){
     if(todoText.trim()==="") return;
 
     let newTodos={
-        id:+data.length+1,
+        id: data.length+1+'',
         text:todoText,
         isEdit:false,
         isCompleted:false,
@@ -25,12 +26,75 @@ function myTodosAdd(){
     .then(()=>alert('data has Been send'))
     .catch((err)=>console.log("this is from addTodo",err))
     .finally(()=>console.log('DOne with api process'))
+
 }
 
 function fetchTodoDB(){
     fetch(API)
     .then((res)=>res.json())
-    .then((res)=>(data=[...res]))
+    .then((res)=>(data=[...res],UIrender()))
     .catch((err)=>console.log(err))
+    
+
+}
+
+function UIrender(){
+    const mainContainer=document.querySelector('#dataInfo');
+    mainContainer.innerHTML = "";
+    if (data.length===0){
+        mainContainer.append('No data');
+        return;
+    } 
+
+    const select=document.getElementById('selectAll')
+    select.innerText="Select All";
+
+    const deselect=document.getElementById('DeSelectAll')
+    deselect.innerText="De Select All";
+
+    data&&data.map((el)=>{
+        const todoDiv=document.createElement('div');
+        const checkInput=document.createElement('input')
+        const headingText=document.createElement('h2')
+        const statusTodo=document.createElement('h3')
+        const editBtn=document.createElement('button')
+        const deleteBtn=document.createElement('button')
+        const id=document.createElement('p')
+        
+        id.innerText=el.id;
+        todoDiv.className="myTodo_div";
+        checkInput.type='checkbox'
+        headingText.innerText=el.text;
+        statusTodo.innerText=el.isEdit ? 'true':'false';
+        editBtn.innerText='edit'
+        deleteBtn.innerText='delete'
+
+
+        select.addEventListener('click',async()=>{
+            await alert("called")
+        })
+
+        editBtn.addEventListener('click',async()=>{
+            await fetch(`${API}/${el.id}`,{
+                method:'PATCH',
+                headers:{
+                    'Content-Type':'application/json',
+                },
+                body:JSON.stringify({isEdit:!el.isEdit}),
+            })
+        })
+
+        deleteBtn.addEventListener('click',async()=>{
+            await fetch(`${API}/${el.id}`,{
+                method:'DELETE',
+                headers:{
+                    'Content-Type':'application/json'
+                }
+            })
+        })
+
+        todoDiv.append(id,checkInput,headingText,editBtn,deleteBtn,statusTodo)
+        mainContainer.append(todoDiv)
+    })
 
 }
